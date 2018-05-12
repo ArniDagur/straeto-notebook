5ceea49542924eb3470fd36d7a39cf8c180dbc1c
Árni Dagur <19227577+ArniDagur@users.noreply.github.com>
2018-05-12 16:41:44 +0000

    moved api to api.py, moved k-finder to straeto.py

--- straeto/straeto.py
+++ straeto/straeto.py
@@ -1,59 +1,37 @@
-import requests
-#from datetime import datetime
+from straeto import api
+import time
 
-_STOKKUR_URL = "https://app.straeto.is/pele/api/v1/"
-#BASE_URL = "https://otp.straeto.is/otp/"
-#ADMIN_URL = "https://admin.straeto.is/api/"
+def getConsecutiveBuses(ratio=1/2):
+    assert 0 <= ratio <= 1
 
-def getAllBusPositions():
-    r = requests.get(_STOKKUR_URL + "positions/all")
-    return r.json()['positions']
+    p_buses = api.getAllBusPositions()
 
-def getBusPositionsFor(buses):
-    r = requests.get(_STOKKUR_URL + "positions/filter/{}".format(",".join(buses)))
-    return r.json()['positions']
+    p_bdict = dict()
+    for pb in p_buses:
+        p_bdict[pb['deviceNumber']] = {'gpsTime':pb['gpsTime'], 'lat':pb['lat'], 'lon':pb['lon'],
+        'speed':pb['speed'], 'route':pb['route']}
 
+    bdict = dict()
+    while True:
+        buses = api.getAllBusPositions()
 
-#def getBusStops():
-#    r = requests.get(BASE_URL + "routers/default/index/stops")
-#    return r.json()
-#
-#def getAllBusForStop(bus_stop, date): #Does this work?
-#    if date == "now":
-#        date = datetime.now().strftime("%d%m%Y")
-#    r = requests.get(BASE_URL + "routers/default/index/stops/{}/stoptimes/{}".format(bus_stop, date))
-#    return r.json()
-#
-#def getTrip(trip_id):
-#    r = requests.get(BASE_URL + "routers/default/index/trips/{}".format(trip_id))
-#    return r.json()
-#
-#def getAreas(language="is"):
-#    r = requests.get(STOKKUR_URL + "areas/{}".format(language))
-#    return r.json()
-#    
-#def getPlan(fromLatLng, toLatLng, time, date, arriveBy, maxPlans, language="is"):
-#    if date == "now":
-#        date = datetime.now().strftime("%d%m%Y")
-#    if time == "now":
-#        time = datetime.now().strftime("%H%M")
-#    r = requests.get(STOKKUR_URL + \
-#                     "routers/default/plan?fromPlace={}&toPlace={}&time={}&date={}&mode={}&arriveBy={}&wheelchair=false&showIntermediateStops=false&numItineraries={}&locale={}"\
-#                     .format(fromLatLng, toLatLng, time, date, arriveBy, maxPlans, language))
-#    return r.json()
-#
-#def getSearch(query, language="is"):
-#    r = requests.get(STOKKUR_URL + "location/{}?search={}".format(language, query))
-#    return r.json()
-#
-#def getReverseSearch(latitude, longitude):
-#    r = requests.get("https://nominatim.openstreetmap.org/reverse?format=json&lat={}&lon={}".format(latitude, longitude))
-#    return r.json()
-#
-#def getTimeTables(slug):
-#    r = requests.get(ADMIN_URL + "timetables/{}".format(slug), headers={'web-language':'is'})
-#    return r.json()
-#
-#def getTimeTableForRoute(schedule_id, route_id, category_id):
-#    r = requests.get(ADMIN_URL + "timetables/{}/{}/{}".format(schedule_id, route_id, category_id), headers={'web-language':'is'})
-#    return r.json()
+        for pb in p_buses:
+            did = pb['deviceNumber']
+            try:
+                b = next(b for b in buses if b['deviceNumber'] == did)
+            except:
+                # Bus is in first sample but not the latter
+                continue
+            if ( # Latter sample is not the same as first + speed is above 2
+            b['gpsTime'] != pb['gpsTime']
+            and b['lat'] != pb['lat'] and b['lon'] != pb['lon']
+            and b['speed'] > 2 and pb['speed'] > 2
+            ):
+                bdict[did] = {'gpsTime':b['gpsTime'], 'lat':b['lat'], 'lon':b['lon'],
+                'speed':b['speed'], 'route':b['route']}
+        
+        if ratio*len(p_bdict) < len(bdict):
+            for key in list(p_bdict):
+                if key not in bdict:
+                    p_bdict.pop(key)
+            return bdict, p_bdict

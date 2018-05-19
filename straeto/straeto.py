b79e01d45dd990255a78c6f59d35c669a224a69b
Árni Dagur <19227577+ArniDagur@users.noreply.github.com>
2018-05-19 01:45:51 +0000

    added getMap()

--- straeto/straeto.py
+++ straeto/straeto.py
@@ -1,5 +1,10 @@
 from straeto import api
 import time
+import os
+from mpl_toolkits.basemap import Basemap
+import pickle
+
+PROJ_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 def getConsecutiveBuses(ratio=1/2):
     assert 0 <= ratio <= 1
@@ -35,3 +40,35 @@ def getConsecutiveBuses(ratio=1/2):
                 if key not in bdict:
                     p_bdict.pop(key)
             return bdict, p_bdict
+
+def getMap(region='reykjavik', res='i', linewidth=0.35, dump=True,
+            color='lightgrey', lake_color='#a7cdf2'):
+    region = region.lower(); res = res.lower()
+    pickle_name = 'map-{}-{}-{}.pickle'.format(region, res, linewidth)
+
+    #Load cached map if it exists
+    if os.path.exists(pickle_name):
+        map = pickle.load(open(pickle_name, 'rb'))
+        return map
+    
+    if region == 'reykjavik':
+        urcrnrlat=64.174820; urcrnrlon=-21.662913
+        llcrnrlat=64.054223; llcrnrlon=-22.075822
+    elif region == 'iceland':
+        urcrnrlat=66.8; urcrnrlon=-12.9
+        llcrnrlat=63.2; llcrnrlon=-25
+    else:
+        raise Exception('Error: Bad region')
+
+    map = Basemap(projection='merc', resolution=res,
+              urcrnrlat=urcrnrlat,urcrnrlon=urcrnrlon,
+              llcrnrlat=llcrnrlat, llcrnrlon=llcrnrlon)
+    map.drawcoastlines(linewidth=linewidth)
+    map.fillcontinents(color=color, lake_color=lake_color)
+    map.drawmapboundary(fill_color=lake_color)
+
+    if dump:
+        pickle.dump(map,
+                    open(pickle_name, 'wb'),
+                    -1) # Format?
+    return map
